ebe8837a618ee9a6d38cbd711f08ead98ad3a4a8
sacredwx <sacredwx@gmail.com>
2023-12-25 15:35:46 +0200

    [Fix] Hardhat Config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,17 +2,22 @@ import { type HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-ethers';
 
 const config: HardhatUserConfig = {
-	solidity: '0.8.19',
+	solidity: '0.8.21',
 	networks: {
 		hardhat: {
 			forking: {
-				url: 'http://ec2-52-90-97-121.compute-1.amazonaws.com:8545',
-			},
+				url: 'http://ec2-54-167-31-43.compute-1.amazonaws.com:8545',
+				blockNumber: 18820117
+			}
+		},
+		external: {
+			url: 'http://ec2-54-167-31-43.compute-1.amazonaws.com:8545',
+			chainId: 31337
 		},
 		localhost: {
-			url: "http://127.0.0.1:8545",
+			url: "http://127.0.0.1:8545"
 		}
-	},
+	}
 };
 
 export default config;
