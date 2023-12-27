2ddfd31086a22f2cbf50f95bdd6b23ac0a13f0a6
sacredwx <sacredwx@gmail.com>
2023-12-27 12:25:57 +0200

    [Fix] AdjustIn Auto Impersonate

--- scripts/changeAdjustInInterval.ts
+++ scripts/changeAdjustInInterval.ts
@@ -3,15 +3,21 @@ import '@nomicfoundation/hardhat-ethers';
 import { MultiPoolStrategy__factory } from '../types/ethers-contracts/factories/scripts/ABIs/MultiPoolStrategy__factory';
 
 async function main() {
+  
+  // Get a signer for the impersonated account
+  
+  const [signer] = await ethers.getSigners();
+  let strategy = MultiPoolStrategy__factory.connect("0xD078a331A8A00AB5391ba9f3AfC910225a78e6A1", signer);
+  
+  const strategyOwner = await strategy.owner();
+  
+  // Get Impersonated Account
   await network.provider.request({
     method: "hardhat_impersonateAccount",
-    params: ["0x93B435e55881Ea20cBBAaE00eaEdAf7Ce366BeF2"]
+    params: [strategyOwner]
   });
-
-  // Get a signer for the impersonated account
   const impersonatedSigner = await ethers.getSigner("0x93B435e55881Ea20cBBAaE00eaEdAf7Ce366BeF2");
-
-  const strategy = MultiPoolStrategy__factory.connect("0xD078a331A8A00AB5391ba9f3AfC910225a78e6A1", impersonatedSigner);
+  strategy = MultiPoolStrategy__factory.connect("0xD078a331A8A00AB5391ba9f3AfC910225a78e6A1", impersonatedSigner);
 
   console.log(await strategy.adjustInInterval());
   console.log(await strategy.owner());
