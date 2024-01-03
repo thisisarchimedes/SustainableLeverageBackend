21ebc1cac67ddf850c2cd35bd3b49ef44dc8e713
sacredwx <sacredwx@gmail.com>
2024-01-03 14:36:32 +0200

    [Fix] AdjustIn Script

--- scripts/changeAdjustInInterval.ts
+++ scripts/changeAdjustInInterval.ts
@@ -1,24 +1,24 @@
 import { ethers, network } from "hardhat";
 import '@nomicfoundation/hardhat-ethers';
-import { MultiPoolStrategy__factory } from '../types/ethers-contracts/factories/scripts/ABIs/MultiPoolStrategy__factory';
 import { FRAXBPALUSD_STRATEGY } from "../test/addresses";
+import { Contracts, EthereumAddress } from "@thisisarchimedes/backend-sdk";
 
 async function main() {
-  
+
   // Get a signer for the impersonated account
-  
+
   const [signer] = await ethers.getSigners();
-  let strategy = MultiPoolStrategy__factory.connect(FRAXBPALUSD_STRATEGY, signer);
-  
+  let strategy = Contracts.general.multiPoolStrategy(new EthereumAddress(FRAXBPALUSD_STRATEGY), signer);
+
   const strategyOwner = await strategy.owner();
-  
+
   // Get Impersonated Account
   await network.provider.request({
     method: "hardhat_impersonateAccount",
     params: [strategyOwner]
   });
   const impersonatedSigner = await ethers.getSigner(strategyOwner);
-  strategy = MultiPoolStrategy__factory.connect(FRAXBPALUSD_STRATEGY, impersonatedSigner);
+  strategy = Contracts.general.multiPoolStrategy(new EthereumAddress(FRAXBPALUSD_STRATEGY), impersonatedSigner);
 
   console.log(await strategy.adjustInInterval());
   console.log(await strategy.owner());
