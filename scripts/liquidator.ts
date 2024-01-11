65d2c3b131930e3cf673ba37adc8b2a2d56f60fb
sacredwx <sacredwx@gmail.com>
2024-01-11 19:12:16 +0200

    [Add] CICD EC2 Deployment

--- scripts/liquidator.ts
+++ scripts/liquidator.ts
@@ -1,5 +1,6 @@
 import { Context } from "aws-lambda/handler";
 import { handler } from "../src/liquidator/lambda-handler";
+import "dotenv/config";
 
 (async () => {
   await handler({}, { callbackWaitsForEmptyEventLoop: false } as Context);
